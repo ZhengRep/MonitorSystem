f7cdf074944e59327b0833bc9a53f2be930126cd
ZhengLi <2726401288@qq.com>
2022-11-21 21:57:22 +0800

    finish StringStorage

--- src/Utils/StringStorage.cpp
+++ src/Utils/StringStorage.cpp
@@ -135,22 +135,22 @@ bool StringStorage::isEqualTo(const StringStorage* other) const
 
 bool StringStorage::split(const TCHAR* delimiters, StringStorage* stringArray, size_t* arrayLength) const
 {
-    //special case for empty string
+    //if this is empty string
     if (this->getLength() == 0) {
         *arrayLength = 0;
         return true;
     }
-    StringStorage copy(this->getString());
+
+    StringStorage copy(getString());
     size_t chunksCount = 0;
-    size_t index = 0;
+    size_t index = -1;
     do {
         StringStorage chunk(_T(""));
         index = copy.findOneOf(delimiters);
-        
-        //case when no delimiter found
-        if ((index == (size_t)-1) && (chunksCount == 0)) {
-            if (arrayLength != 0) {
-                if (stringArray != 0) {
+
+        if (index == (size_t)-1 && chunksCount == 0) {
+            if (arrayLength != NULL) {
+                if (stringArray != NULL) {
                     if (*arrayLength < 1) {
                         return false;
                     }
@@ -161,9 +161,10 @@ bool StringStorage::split(const TCHAR* delimiters, StringStorage* stringArray, s
                 return false;
             }
 
-            if (stringArray != 0) {
+            if (stringArray != NULL) {
                 stringArray->setString(getString());
             }
+
             return true;
         }
 
@@ -175,10 +176,11 @@ bool StringStorage::split(const TCHAR* delimiters, StringStorage* stringArray, s
         }
 
         copy.getSubString(&copy, index + 1, copy.getLength() - 1);
-        if ((stringArray != NULL) && (chunksCount >= *arrayLength)) {
+
+        if (stringArray != NULL && chunksCount >= *arrayLength) {
             return false;
         }
-        else if (stringArray != NULL) {
+        else if(stringArray != NULL) {
             stringArray[chunksCount] = chunk;
         }
 
@@ -191,68 +193,145 @@ bool StringStorage::split(const TCHAR* delimiters, StringStorage* stringArray, s
 
 TCHAR* StringStorage::find(const TCHAR* subStr)
 {
-    return nullptr;
+    return _tcsstr(&m_buffer.front(), subStr);
 }
 
 size_t StringStorage::findChar(const TCHAR c)
 {
-    return size_t();
+    size_t length = getLength();
+    for (size_t i = 0; i < length; i++) {
+        if (m_buffer[i] == c) {
+            return i;
+        }
+    }
+    return (size_t)-1;
 }
 
+//returns first position of any character that string argument have
 size_t StringStorage::findOneOf(const TCHAR* string)
 {
-    return size_t();
+    size_t length = getLength();
+    size_t argLength = _tcslen(string);
+    for (size_t i = 0; i < length; i++) {
+        for (size_t j = 0; j < argLength; j++) {
+            if (m_buffer[i] == string[j]) {
+                return i;
+            }
+        }
+    }
+    return (size_t) - 1;
 }
 
 size_t StringStorage::findLast(const TCHAR c)
 {
-    return size_t();
+    for (size_t i = getLength() - 1; i + 1 != 0; i--) {
+        if (m_buffer[i] == c) {
+            return i;
+        }
+    }
+    return (size_t)-1;
 }
 
 void StringStorage::removeChars(const TCHAR badCharacters[], size_t count)
 {
+    size_t j = 0;
+    size_t length = getLength();
+
+    for (size_t i = 0; i < length; i++) {
+        TCHAR each = m_buffer[i];
+        bool badCharacter = false;
+        for (size_t k = 0; k < count; k++) {
+            if (each == badCharacters[k]) {
+                badCharacter = true;
+                break;
+            }
+        }
+        if (!badCharacter) {
+            m_buffer[j++] = each;
+        }
+    }
+
+    m_buffer[j] = _T('\0');
+    m_buffer.resize(j + 1);
 }
 
 void StringStorage::remove(size_t startIndex, size_t count)
 {
+    bool isFaild = startIndex + count > getLength();
+    _ASSERT(!isFaild); //is true go on
+    if (isFaild) { //release version
+        throw Exception(_T("An incorrect StringStorage::remove() usage"));
+    }
+    BufferType newBuffer = m_buffer;
+    size_t copyCount = getSize() - (startIndex + count) * sizeof(TCHAR);
+    memcpy(&newBuffer[startIndex], &newBuffer[startIndex + count], copyCount);
+    setString(&newBuffer.front());
+
 }
 
 void StringStorage::truncate(size_t count)
 {
+    count = std::min(getLength(), count);
+    remove(getLength() - count, count);
 }
 
 void StringStorage::toLowerCase()
 {
+    size_t length = getLength();
+    for (size_t i = 0; i < length; i++) {
+        if (_istalpha(m_buffer[i])) {
+            m_buffer[i] = _totlower(m_buffer[i]);
+        }
+    }
 }
 
 void StringStorage::toUpperCase()
 {
+    _tcsupr_s(&m_buffer.front(), getLength() + 1);
 }
 
 void StringStorage::format(const TCHAR* format, ...)
 {
+    va_list vl;
+    va_start(vl, format);
+
+    int count = _vsctprintf(format, vl);
+    va_end(vl);
+
+    resizeBuffer(count + 1);
+    va_start(vl, format);
+    _vstprintf_s(&m_buffer.front(), count + 1, format, vl);
+    va_end(vl);
 }
 
 void StringStorage::operator=(const StringStorage& other)
 {
+    setString(other.getString());
 }
 
 bool StringStorage::operator==(const StringStorage& str) const
 {
-    return false;
+    return isEqualTo(&str);
 }
 
 bool StringStorage::operator<(const StringStorage& str) const
 {
-    return false;
+    return _tcscmp(getString(), str.getString()) < 0;
 }
 
 void StringStorage::operator+=(const TCHAR* str)
 {
+    appendString(str);
 }
 
 void StringStorage::replaceChar(TCHAR oldChar, TCHAR newChar)
 {
+    size_t length = getLength();
+    for (size_t i = 0; i < length; i++) {
+        if (m_buffer[i] == oldChar) {
+            m_buffer[i] = newChar;
+        }
+    }
 }
 
 void StringStorage::resizeBuffer(size_t newSize)
---
+++ src/test/UtilsTest/TestStringStorage.cpp
@@ -0,0 +1,14 @@
+//#include "../../Utils/StringStorage.h"
+
+int main()
+{
+	
+	/*StringStorage str(_T("Hello World. I am Zheng!"));
+	StringStorage words[10];
+	size_t arrayLength = 10;
+	str.split(_T(" "), words, &arrayLength);*/
+
+
+
+	return 0;
+}
