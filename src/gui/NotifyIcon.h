f2dd552bda4ee3b9c769808ee8f5471100b7b8de
ZhengLi <2726401288@qq.com>
2023-01-13 15:53:35 +0800

    add NotifyIcon

---
+++ src/gui/NotifyIcon.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "NotifyIconWindow.h"
+#include "drawing/Icon.h"
+
+#include<shellapi.h>
+
+class NotifyIcon: NotifyIconWindow
+{
+public:
+	NotifyIcon(bool showAfterCreation = true);
+	virtual ~NotifyIcon();
+
+	const Icon* getIcon() const;
+	bool isVisible() const;
+
+	void setIcon(Icon* icon);
+	void setText(const TCHAR* text);
+	void showBallon(const TCHAR* message, const TCHAR* caption, DWORD timeoutMillis);
+	void show();
+	void hide();
+
+protected:
+	NOTIFYICONDATA m_nid;
+	Icon* m_icon;
+	bool m_visible;
+};
+
